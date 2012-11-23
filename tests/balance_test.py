ee6d15a0b2814c41f8a93bbd899c11f39e8a5f71
Alex Morega <alex@grep.ro>
2012-11-23 18:57:36 +0200

    compute consumption

--- tests/balance_test.py
+++ tests/balance_test.py
@@ -3,12 +3,22 @@ from decimal import Decimal as D
 from collections import defaultdict
 
 
+CENT = D('.01')
+
+
 def compute(history):
     balance = defaultdict(D)
 
-    for day_values in history['contributions'].values():
+    for day_values in history.get('contributions', {}).values():
         for name, value in day_values.items():
-            balance[name] += D(value).quantize(D('.01'))
+            balance[name] += D(value).quantize(CENT)
+
+    for day_orsers in history.get('orders', {}).values():
+        for order in day_orsers:
+            per_eat = (D(order['price']) / D(order['qty'])).quantize(CENT)
+            for day_eats in order['eat'].values():
+                for name, value in day_eats.items():
+                    balance[name] -= D(value) * per_eat
 
     return dict(balance)
 
@@ -24,3 +34,21 @@ class BalanceTest(unittest.TestCase):
         }
         balance = compute(history)
         self.assertEqual(balance['anton'], D('22.15'))
+
+    def test_compute_sum_of_consumptions(self):
+        history = {
+            'orders': {
+                '2012-11-20': [
+                  {'price': 55,
+                   'qty': 10,
+                    'eat': {'2012-11-20': {'anton': 1}}},
+                ],
+                '2012-11-23': [
+                  {'price': 75,
+                   'qty': 7,
+                    'eat': {'2012-11-20': {'anton': 2}}},
+                ],
+            }
+        }
+        balance = compute(history)
+        self.assertEqual(balance['anton'], D('-26.92'))
