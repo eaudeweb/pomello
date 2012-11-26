1047561fb77f5f9da95c4e3df608f90638adc286
Alex Morega <alex@grep.ro>
2012-11-26 16:36:35 +0200

    show balance of everyone

--- manage.py
+++ manage.py
@@ -3,6 +3,12 @@
 import os
 import flask
 from flask.ext.script import Manager
+from path import path
+import yaml
+import balance
+
+
+HISTORY_DIR = path(__file__).abspath().parent / 'history'
 
 
 views = flask.Blueprint(__name__, 'views')
@@ -10,7 +16,10 @@ views = flask.Blueprint(__name__, 'views')
 
 @views.route('/')
 def home():
-    return flask.render_template('overview.html')
+    with (HISTORY_DIR / '2012-11.yaml').open('rb') as f:
+        history = yaml.load(f)
+    results = balance.compute(history)
+    return flask.render_template('overview.html', results=results)
 
 
 @views.app_template_filter('py3format')
