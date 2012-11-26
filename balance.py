a756de1a370022b008b252d90cf0aa4b31589de2
Alex Morega <alex@grep.ro>
2012-11-26 15:43:54 +0200

    move `compute` to `balance.py`

---
+++ balance.py
@@ -0,0 +1,37 @@
+from decimal import Decimal as D
+from collections import defaultdict
+
+
+CENT = D('.01')
+
+
+def compute(history):
+    results = defaultdict(lambda: {'balance': D(0), 'history': []})
+
+    for label, day_values in history.get('contributions', {}).items():
+        for name, raw_value in day_values.items():
+            entry = results[name]
+            value = D(raw_value).quantize(CENT)
+            entry['balance'] += value
+            history_item = {
+                'description': label,
+                'value': value.quantize(CENT),
+            }
+            entry['history'].append(history_item)
+
+    for day_orsers in history.get('orders', {}).values():
+        for order in day_orsers:
+            per_eat = (D(order['price']) / D(order['qty'])).quantize(CENT)
+            for eat_date, day_eats in order['eat'].items():
+                for name, raw_value in day_eats.items():
+                    value = - D(raw_value).quantize(CENT) * per_eat
+                    entry = results[name]
+                    entry['balance'] += value
+                    history_item = {
+                        'value': value.quantize(CENT),
+                        'description': ('{order[name]} {eat_date}'
+                                        .format(**locals())),
+                    }
+                    entry['history'].append(history_item)
+
+    return dict(results)
--- tests/balance_test.py
+++ tests/balance_test.py
@@ -1,46 +1,11 @@
 import unittest
 from decimal import Decimal as D
-from collections import defaultdict
-
-
-CENT = D('.01')
-
-
-def compute(history):
-    results = defaultdict(lambda: {'balance': D(0), 'history': []})
-
-    for label, day_values in history.get('contributions', {}).items():
-        for name, raw_value in day_values.items():
-            entry = results[name]
-            value = D(raw_value).quantize(CENT)
-            entry['balance'] += value
-            history_item = {
-                'description': label,
-                'value': value.quantize(CENT),
-            }
-            entry['history'].append(history_item)
-
-    for day_orsers in history.get('orders', {}).values():
-        for order in day_orsers:
-            per_eat = (D(order['price']) / D(order['qty'])).quantize(CENT)
-            for eat_date, day_eats in order['eat'].items():
-                for name, raw_value in day_eats.items():
-                    value = - D(raw_value).quantize(CENT) * per_eat
-                    entry = results[name]
-                    entry['balance'] += value
-                    history_item = {
-                        'value': value.quantize(CENT),
-                        'description': ('{order[name]} {eat_date}'
-                                        .format(**locals())),
-                    }
-                    entry['history'].append(history_item)
-
-    return dict(results)
 
 
 class BalanceTest(unittest.TestCase):
 
     def test_compute_sum_of_contributions(self):
+        from balance import compute
         history = {
             'contributions': {
                 'initial': {'anton': 13},
@@ -51,6 +16,7 @@ class BalanceTest(unittest.TestCase):
         self.assertEqual(results['anton']['balance'], D('22.15'))
 
     def test_compute_sum_of_consumptions(self):
+        from balance import compute
         history = {
             'orders': {
                 '2012-11-20': [
@@ -71,6 +37,7 @@ class BalanceTest(unittest.TestCase):
         self.assertEqual(results['anton']['balance'], D('-26.92'))
 
     def test_compute_saves_personal_history_entries(self):
+        from balance import compute
         history = {
             'contributions': {
                 'initial': {'anton': 13},
