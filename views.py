411a597336650429842fb3c651226cc35920173a
Alex Morega <alex@grep.ro>
2012-11-27 19:51:59 +0200

    show table with history

--- views.py
+++ views.py
@@ -1,4 +1,5 @@
 import flask
+from datetime import date
 from path import path
 import yaml
 import balance
@@ -8,18 +9,25 @@ HISTORY_DIR = path(__file__).abspath().parent / 'history'
 views = flask.Blueprint(__name__, 'views')
 
 
-@views.route('/')
-def home():
+def _get_balance():
     with (HISTORY_DIR / '2012-11.yaml').open('rb') as f:
         history = yaml.load(f)
-    results = balance.compute(history)
+    return balance.compute(history)
+
+
+@views.route('/')
+def home():
+    results = _get_balance()
     return flask.render_template('overview.html', results=results)
 
 
 @views.route('/person/<string:name>')
 def person(name):
+    results = _get_balance()
     return flask.render_template('person.html', **{
         'name': name,
+        'history': sorted(results[name]['history'],
+                          key=lambda h: h['date'] or date(2000, 1, 1)),
     })
 
 
