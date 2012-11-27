085ae5bf1977af4995d73760d51be4f32342a9fa
Alex Morega <alex@grep.ro>
2012-11-27 20:44:25 +0200

    color display of monetary values

--- views.py
+++ views.py
@@ -2,6 +2,7 @@ import flask
 from datetime import date
 from path import path
 import yaml
+import jinja2
 import balance
 
 HISTORY_DIR = path(__file__).abspath().parent / 'history'
@@ -29,3 +30,11 @@ def person(name):
         'history': sorted(results[name]['history'],
                           key=lambda h: h['date'] or date(2000, 1, 1)),
     })
+
+
+@views.app_template_filter('money')
+def money(value):
+    css_class = 'money-negative' if value < 0 else 'money-positive'
+    value = abs(value)
+    return jinja2.Markup('<span class="{css_class}">{value:.2f}</span>'
+                         .format(**locals()))
